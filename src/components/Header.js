3a6a4ca23714f9d4a48f6536ee9ffc83235de072
Victor Chan <vctrchn@hotmail.ca>
2023-04-28 17:48:14 -0400

    working on export pdf function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,33 @@
 import react, { Component } from "react";
 import "../styles/Header.css";
+import JsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 class Header extends Component {
   constructor(props) {
     super(props);
+
+    this.generatePDF = this.generatePDF.bind(this);
+  }
+
+  generatePDF() {
+    const input = document.querySelector(".resume");
+
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new JsPDF("portrait", "pt", "a4");
+      pdf.addImage(imgData, "PNG", 0, 0);
+      pdf.save("report.pdf");
+    });
   }
 
   render() {
     return (
       <div className="header">
         <h1 className="app-Title">r/Resume Builder</h1>
+        <div className="exportPDF" onClick={this.generatePDF}>
+          Export as PDF
+        </div>
       </div>
     );
   }
